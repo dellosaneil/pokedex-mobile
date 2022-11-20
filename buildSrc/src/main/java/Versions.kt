42481694573508c94654ac5d813dcf66f02703b8
dellosaneil <dellosaneil@gmail.com>
2022-11-20 11:01:56 +0800

    added dependencies for Desination Navigation

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -18,6 +18,8 @@ object Versions {
         const val composeActivityVersion = "1.6.1"
         const val glideVersion = "2.1.0"
         const val timberVersion = "5.0.1"
+        const val navigationVersion = "1.7.27-beta"
+        const val googleKspVersion = "1.7.20-1.0.8"
     }
 }
 
