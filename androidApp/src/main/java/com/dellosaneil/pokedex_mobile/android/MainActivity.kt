6dcb81e26b7ed572dcf78eaaad1491607c12cba0
dellosaneil <dellosaneil@gmail.com>
2022-11-20 13:40:08 +0800

    added test for pagination

--- androidApp/src/main/java/com/dellosaneil/pokedex_mobile/android/MainActivity.kt
+++ androidApp/src/main/java/com/dellosaneil/pokedex_mobile/android/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.dellosaneil.pokedex_mobile.android.theme.PokedexTheme
-import com.dellosaneil.pokedex_mobile.android.ui.NavGraphs
+import com.dellosaneil.pokedex_mobile.android.ui.pokemonlist.NavGraphs
 import com.ramcosta.composedestinations.DestinationsNavHost
 
 class MainActivity : ComponentActivity() {
--- androidApp/src/main/java/com/dellosaneil/pokedex_mobile/android/ui/pokemonlist/PokemonListScreen.kt
+++ androidApp/src/main/java/com/dellosaneil/pokedex_mobile/android/ui/pokemonlist/PokemonListScreen.kt
@@ -1,12 +1,16 @@
-package com.dellosaneil.pokedex_mobile.android.ui
+package com.dellosaneil.pokedex_mobile.android.ui.pokemonlist
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
+import com.dellosaneil.pokedex_mobile.android.mvvm.pokemonlist.PokemonListViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
+import org.koin.androidx.compose.koinViewModel
 
 @RootNavGraph(start = true)
 @Destination
@@ -14,7 +18,11 @@ import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 fun PokemonListScreen(
     navigator: DestinationsNavigator,
 ) {
-    Text("hello")
+    val viewModel : PokemonListViewModel = koinViewModel()
+    val viewState by viewModel.viewState.collectAsState()
+    if(viewState.pokemonList.isNotEmpty()) {
+        Text(text = viewState.pokemonList.first().name)
+    }
 }
 
 
