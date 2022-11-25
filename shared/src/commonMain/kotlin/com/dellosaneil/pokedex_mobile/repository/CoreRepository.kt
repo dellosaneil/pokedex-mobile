ba8428456559a2e0877f525e3a530666ad1d6729
dellosaneil <dellosaneil@gmail.com>
2022-11-25 17:48:55 +0800

    create PokemonDetail query

--- shared/src/commonMain/kotlin/com/dellosaneil/pokedex_mobile/repository/CoreRepository.kt
+++ shared/src/commonMain/kotlin/com/dellosaneil/pokedex_mobile/repository/CoreRepository.kt
@@ -1,7 +1,9 @@
 package com.dellosaneil.pokedex_mobile.repository
 
+import com.dellosaneil.pokedex_mobile.model.PokemonDetail
 import com.dellosaneil.pokedex_mobile.model.pokemonlist.PreviewPokemon
 
 interface CoreRepository {
     suspend fun fetchPokemonList(isInitialLoad: Boolean): List<PreviewPokemon>
+    suspend fun fetchPokemonDetail(id: Int) : PokemonDetail
 }
